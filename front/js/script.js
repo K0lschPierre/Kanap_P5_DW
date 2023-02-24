24477a148fbc482ea303fd7afa76829c64bd857c
Pierre KOLSCH <kolsch.pierre@gmail.com>
2023-02-24 21:09:53 +0100

    Code formatter

--- front/js/script.js
+++ front/js/script.js
@@ -1,48 +1,48 @@
 // Nous sélectionnons l'emplacement où nous montrerons nos produits.
-const getData = async (endpoint= "http://localhost:3000/api/products") => {
-    try {
-        return await (await fetch(endpoint)).json()
+const getData = async (endpoint = "http://localhost:3000/api/products") => {
+  try {
+    return await (await fetch(endpoint)).json();
     // On utilise un message d'excuse en cas d'erreur.
-    } catch (error) {
-        console.error(error)
-        alert("Pas de données, veuillez nous excuser.");
-    }
-}
+  } catch (error) {
+    console.error(error);
+    alert("Pas de données, veuillez nous excuser.");
+  }
+};
 
 const chargerProduit = (products) => {
-const Items = document.querySelector('#items');
-    // On placera ces données dans une constante Product.
-    for (const Product of products){
-        console.log(Product);
+  const Items = document.querySelector("#items");
+  // On placera ces données dans une constante Product.
+  for (const Product of products) {
+    console.log(Product);
 
-        // On produit les éléments HTML de la page "index.html" pour ensuite y insérer les données récupérées de l'API.
-        let newLink = document.createElement('a');
-        newLink.href = `./product.html?id=${Product._id}`;
-        Items.appendChild(newLink);
+    // On produit les éléments HTML de la page "index.html" pour ensuite y insérer les données récupérées de l'API.
+    let newLink = document.createElement("a");
+    newLink.href = `./product.html?id=${Product._id}`;
+    Items.appendChild(newLink);
 
-        let newArticle = document.createElement('article');
-        newLink.appendChild(newArticle);
+    let newArticle = document.createElement("article");
+    newLink.appendChild(newArticle);
 
-        let newImg = document.createElement('img');
-        newImg.setAttribute("src", Product.imageUrl);
-        newImg.setAttribute("alt", Product.altTxt);
-        newArticle.appendChild(newImg);
+    let newImg = document.createElement("img");
+    newImg.setAttribute("src", Product.imageUrl);
+    newImg.setAttribute("alt", Product.altTxt);
+    newArticle.appendChild(newImg);
 
-        let newH3 = document.createElement('h3');
-        newH3.setAttribute("class", "productName");
-        newH3.innerText = Product.name;
-        newArticle.appendChild(newH3);
+    let newH3 = document.createElement("h3");
+    newH3.setAttribute("class", "productName");
+    newH3.innerText = Product.name;
+    newArticle.appendChild(newH3);
 
-        let newP = document.createElement('p')
-        newP.setAttribute("class", "productDescription");
-        newP.innerText = Product.description;
-        newArticle.appendChild(newP);
-    }
-}
+    let newP = document.createElement("p");
+    newP.setAttribute("class", "productDescription");
+    newP.innerText = Product.description;
+    newArticle.appendChild(newP);
+  }
+};
 // On crée une fonction asynchrone auto-exécutée.
-// (1) Appelle la fonction "getData()" qui retourne une promesse.
-// (2) Attend la résolution de cette promesse avec "await".
+// (1) Appelle la fonction "getData()" qui retourne une promesse,
+// (2) Attend la résolution de cette promesse avec "await",
 // (3) Passe la valeur résolue à la fonction "chargerProduit" en tant qu'argument.
 (async () => {
-    chargerProduit(await getData())
-})()
+  chargerProduit(await getData());
+})();
